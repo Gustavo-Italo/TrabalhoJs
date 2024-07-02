1280a91f459a490b6007e50ddca38116c96a4445
Gferracioli <gustavo.freelancer.site@gmail.com>
2024-07-02 13:44:00 -0300

    adicionei as imagens que faltavam

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ class Cerveja {
 
 const brahmaTradLata = new Cerveja ("Brahma", "Brasil", "Pilsen", 3.10, 350, "./img/brahma_lata.png");
 const brahmaTradLong = new Cerveja ("Brahma", "Brasil", "Pilsen", 4.79, 355, "./img/brahma_ln.png");
-const coronaExtraLong = new Cerveja ("Corona", "México", "American Lager", 5.99, 330, "./img/corona.jpg");
+const coronaExtraLong = new Cerveja ("Corona", "México", "American Lager", 5.99, 330, "./img/corona_ln.png");
 const antarcticaTradLata = new Cerveja ("Artarctica", "Brasil", "Pilsen", 3.10, 350, "./img/antartica_lata.png");
 const antarcticaTradLong = new Cerveja ("Antarctica", "Brasil", "Pilsen", 4.79, 355, "./img/antartica_ln.png");
 const spatenLong = new Cerveja ("Spaten", "Alemanha", "Munich Helles", 5.99, 355, "./img/spaten_lata.jpeg");
@@ -31,7 +31,7 @@ class Vinho {
 const chacChacTinto = new Vinho ("Chac Chac", "Argentina", "Tinto Malbec", 40.99, 750, "./img/chacchac_tinto.png");
 const chacChacBranco = new Vinho ("Chac Chac", "Argentina", "Sauvignon Blanc", 40.99, 750, "./img/chacchac_souvignon.png");
 const trapicheTinto = new Vinho ("Trapiche", "Argentina", "Tinto Merlot", 51.99, 750, "./img/trapiche_tinto.png");
-const periquitaRose = new Vinho ("Periquita", "Portugal", "Rosé Touriga Nacional", 69.99, "./img/periquita_rose.png");
+const periquitaRose = new Vinho ("Periquita", "Portugal", "Rosé Touriga Nacional", 69.99, 750, "./img/periquita_rose.png");
 const periquitaTinto = new Vinho ("Periquita", "Portugal", "Tinto Seco", 79.99, 750, "./img/periquita_seco.png");
 const novecentoTinto = new Vinho ("Concha Y Toro", "Chile", "Tinto Carménère Reservado", 35.99, 750, "./img/conchaytoro_carmenere.png"); 
 const santaHelenaRose = new Vinho ("Santa Helena", "Chile", "Rosé Reservado Cabernet Sauvignon", 41.19, 750, "./img/santahelena_rose.png");
@@ -50,11 +50,11 @@ class Refrigerante {
 
 const cocaCola = new Refrigerante ("Coca Cola", "Canela", 7.50, 2, "./img/cocacola.jpeg");
 const guaranaAntarctica = new Refrigerante ("Guaraná Antarctica", "Guaraná", 8.00, 2, "./img/guarana.jpeg");
-const pepsi = new Refrigerante ("Pepsi", "Canela", 7.70, 2);
+const pepsi = new Refrigerante ("Pepsi", "Canela", 7.70, 2, "./img/pepsi_lata.png");
 const fantaUva = new Refrigerante ("Fanta", "Uva", 7.00, 2, "./img/fanta_uva.jpeg");
 const fantaLaranja = new Refrigerante ("Fanta", "Laranja", 7.00, 2, "./img/fanta_laranja.png");
 const sprite = new Refrigerante ("Sprite", "Limão", 5.00, 2, "./img/sprite_lata.png");
-const kuat = new Refrigerante ("Kuat", "Guaraná", 5.50, 2);
+const kuat = new Refrigerante ("Kuat", "Guaraná", 5.50, 2, "./img/kuat_lata.png");
 
 
 class Agua {
@@ -68,10 +68,10 @@ class Agua {
 }
 
 const crystal = new Agua ("Crystal", "Mineral", 2.00, 350, "./img/crystal_mineral.png");
-const ibira = new Agua ("Ibirá", "Mineral", 1.50, 350);
-const minalba = new Agua ("Minalba", "Mineral", 1.65, 350);
-const cristal = new Agua ("Cristal", "Água com gás", 2.50, 350, "./img/crystal_gas.png");
-const bioleve = new Agua ("Bioleve", "Água com gás", 2.75, 350);
+const ibira = new Agua ("Ibirá", "Mineral", 1.50, 500, "./img/ibira_mineral.png");
+const minalba = new Agua ("Minalba", "Mineral", 1.65, 500, "./img/minalba_mineral.png");
+const cristal = new Agua ("Cristal", "Água com gás", 2.50, 500, "./img/crystal_gas.png");
+const bioleve = new Agua ("Bioleve", "Água com gás", 2.75, 500, "./img/bioleve_gas.png");
 
 
 const bebidasDisponiveis = [
