f345374b96727522c3fa7ed8f96a5a6cc9f4ed3c
Gferracioli <gustavo.freelancer.site@gmail.com>
2024-07-09 11:33:32 -0300

    mudança para uso do localStorage e arquivo base o bebidas.json

--- filtro.js
+++ filtro.js
@@ -1,41 +1,42 @@
-document.getElementById('categoria-cerveja').addEventListener('click', () => filtrarCategoria('Cerveja'));
-document.getElementById('categoria-vinho').addEventListener('click', () => filtrarCategoria('Vinho'));
-document.getElementById('categoria-refrigerante').addEventListener('click', () => filtrarCategoria('Refrigerante'));
-document.getElementById('categoria-agua').addEventListener('click', () => filtrarCategoria('Água'));
-document.getElementById('categoria-todas').addEventListener('click', () => filtrarCategoria('Todas'))
+// document.getElementById('categoria-cerveja').addEventListener('click', () => filtrarCategoria('cerveja'));
+// document.getElementById('categoria-vinho').addEventListener('click', () => filtrarCategoria('vinho'));
+// document.getElementById('categoria-refrigerante').addEventListener('click', () => filtrarCategoria('refrigerante'));
+// document.getElementById('categoria-agua').addEventListener('click', () => filtrarCategoria('agua'));
+// document.getElementById('categoria-todas').addEventListener('click', () => filtrarCategoria('Todas'));
+
+// function filtrarCategoria(categoria) {
+//     let categoriaClasse;
+//     switch (categoria) {
+//         case 'cerveja':
+//             categoriaClasse = 'cerveja';
+//             break;
+//         case 'vinho':
+//             categoriaClasse = 'vinho';
+//             break;
+//         case 'refrigerante':
+//             categoriaClasse = 'refrigerante';
+//             break;
+//         case 'agua':
+//             categoriaClasse = 'agua';
+//             break;
+//         case 'Todas':
+//             categoriaClasse = '';
+//             break;
+//         default:
+//             categoriaClasse = '';
+//     }
+//     gerarCards(categoriaClasse);
+// }
+
+// gerarCards();
+document.getElementById('categoria-cerveja').addEventListener('click', () => filtrarCategoria('cerveja'));
+document.getElementById('categoria-vinho').addEventListener('click', () => filtrarCategoria('vinho'));
+document.getElementById('categoria-refrigerante').addEventListener('click', () => filtrarCategoria('refrigerante'));
+document.getElementById('categoria-agua').addEventListener('click', () => filtrarCategoria('agua'));
+document.getElementById('categoria-todas').addEventListener('click', () => filtrarCategoria(''));
 
 function filtrarCategoria(categoria) {
-    let categoriaClasse;
-    switch (categoria) {
-        case 'Cerveja':
-            categoriaClasse = 'Cerveja';
-            break;
-        case 'Vinho':
-            categoriaClasse = 'Vinho';
-            break;
-        case 'Refrigerante':
-            categoriaClasse = 'Refrigerante';
-            break;
-        case 'Água':
-            categoriaClasse = 'Agua';
-            break;
-        case 'Todas':
-            categoriaClasse = '';
-            break;
-        default:
-            categoriaClasse = '';
-    }
-    gerarCards(categoriaClasse);
+    gerarCards(categoria);
 }
 
 gerarCards();
-
-// function filterProducts(produtos, categoria) {
-//     if (!categoria) {
-//       return produtos; // Retorna todos os produtos se a categoria estiver vazia
-//     }
-//     return produtos.filter(produto => 
-//       produto.categoria.toLowerCase().includes(categoria.toLowerCase())
-//     );
-//   }
-  
